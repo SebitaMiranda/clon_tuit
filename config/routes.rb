c3d123b1eb93205028b75f2f5fb124aa400587a3
SebitaMiranda <seba.miranda1980@gmail.com>
2023-07-25 12:37:16 -0400

    cambio de color de fondo y letra

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
   resources :clon_tuits
 
-  member do 
-    get "preview"
-  end
-
-  collection do 
-    get "search"
-  end
-end
-  
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "clon_tuits#index"
 
+end
